8a596bb41d65969c7d09a60fcfd936fa0f1ce94b
phat230 <phay123321@gmail.com>
2025-04-13 15:04:19 +0700

    Mô tả thay đổi

--- myproject/posts/admin.py
+++ myproject/posts/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'date')
+    list_filter = ('date',)
+    search_fields = ('title', 'body')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'body')
+        }),
+        ('Route Information', {
+            'fields': ('points_outbound', 'points_inbound')
+        }),
+        ('Metadata', {
+            'fields': ('date',)
+        }),
+    )
+    readonly_fields = ('date',)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # Editing an existing object
+            return self.readonly_fields + ('slug',)
+        return self.readonly_fields
